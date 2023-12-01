1d92b25405686e7c4f3f60d4eff8ca876975afd4
root <david.macdonald@konghq.com>
2023-12-01 15:49:33 +0000

    dev

--- src/views/MyCerts.vue
+++ src/views/MyCerts.vue
@@ -7,7 +7,7 @@
 
     <p>Please enter the CN from your certificate:</p>
 
-    <form action="http://localhost:8000/consumers" method="post" target="_blank">
+    <form action="https://localhost:8443/consumers" method="post" target="_blank">
       <label for="uname">Username:</label>
       <input type="text" id="uname" name="uname"><br><br>
       <label for="cn">Common Name:</label>
