da2003f73fd293a411373de4dc7f6b15d2731bc9
root <david.macdonald@konghq.com>
2023-12-04 09:52:48 +0000

    dev

--- src/views/ProductCatalogWrapper.vue
+++ src/views/ProductCatalogWrapper.vue
@@ -4,7 +4,7 @@
       class="products-top-section flex flex-col items-center justify-center py-16 bg-section_colors-hero"
       :style="catalog_cover_style"
     >
-      <h4 class="products-welcome mb-4 font-normal color-text_colors-secondary text-2xl">
+      <h4 class="products-welcome mb-4 font-normal color-text_colors-secondary text-6xl">
         {{ welcome_message }}
       </h4>
       <h1 class="products-title mb-5 font-normal color-text_colors-hero text-4xl">
